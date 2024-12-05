5a6ec9453223ea7beb876ebbf280029de6ee8987
RobertoMarcelino98 <dsm21190296.lmarcelino@alumnos.utsv.edu.com>
2024-12-04 18:58:52 -0600

    nuevo sin imagens

--- controller/zapateriaController.js
+++ controller/zapateriaController.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 // Configuración de multer para guardar archivos
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, Date.now() + path.extname(file.originalname));
     }
 });
